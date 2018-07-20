14f85bf716635f320bbb6d62b4186653b35ba501
tim t <tim.telf+githubFEep@gmail.com>
2018-07-20 13:59:25 +0300

    Update Hello.java

--- src/main/java/com/example/hello/Hello.java
+++ src/main/java/com/example/hello/Hello.java
@@ -3,7 +3,7 @@ package com.example.hello;
 public class Hello {
 	
 	public static void main(String [] args) {
-		System.out.println("Hello, world!");
+		System.out.println("Hello, world! YES!!");
 	}
 	
 }
